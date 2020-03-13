ef451f541b4c17f38ab23f87f5afa5023266232b
Lumby98 <lumby89@gmail.com>
2020-03-13 15:49:16 +0100

    idk

--- src/attendance/automation/dal/DatabaseConnecter.java
+++ src/attendance/automation/dal/DatabaseConnecter.java
@@ -5,11 +5,46 @@
  */
 package attendance.automation.dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  *
  * @author lumby
  */
 public class DatabaseConnecter
 {
-    
+    private SQLServerDataSource dataSource;
+
+    /**
+     * DatabaseConnector constructor
+     * the class takes the info from the DBSettings.txt file and establishes a connection to the database. 
+     * @throws java.io.FileNotFoundException
+     * @throws IOException
+     */
+    public DatabaseConnecter() throws FileNotFoundException, IOException{
+        Properties props = new Properties();
+       
+        props.load(new FileReader("DBSettings.txt"));
+        
+        dataSource = new SQLServerDataSource();
+        dataSource.setDatabaseName(props.getProperty("database"));
+        dataSource.setUser(props.getProperty("user"));
+        dataSource.setPassword(props.getProperty("password"));
+        dataSource.setServerName(props.getProperty("server"));
+    }
+
+    /**
+     * Get the connection to the database
+     *
+     * @return Database connection
+     */
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
 }
